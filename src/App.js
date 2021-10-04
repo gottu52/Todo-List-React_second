8ff9d12c9054971867412d57e3ec89f2e75db30f
gottu52 <89571642+gottu52@users.noreply.github.com>
2021-10-04 19:43:02 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import "./styles.css";
+import React from "react";
 
-export default function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      {/* 入力欄 */}
+      <div class="area" id="input-area">
+        <input placeholder="入力しろ" />
+        <button>追加</button>
+      </div>
+      {/* 未完了リスト */}
+      <div class="area" id="incomplete-area">
+        <h1>未完了リスト</h1>
+        <ul>
+          <div class="list-row">
+            <li>a</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      {/* 完了リスト */}
+      <div class="area" id="complete-area">
+        <h1>完了リスト</h1>
+        <ul>
+          <div class="list-row">
+            <li>i</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
   );
-}
+};
